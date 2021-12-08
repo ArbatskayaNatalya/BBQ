f71409ab08bc146a69842f41b2507a04b16c5395
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2021-12-08 20:36:41 +0300

    change device form with bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,10 +8,13 @@ class ApplicationController < ActionController::Base
       :account_update,
       keys: [:password, :password_confirmation, :current_password]
     )
+    devise_parameter_sanitizer.permit(
+      :sign_up,
+      keys: [:name]
+      )
   end
 
   def current_user_can_edit?(event)
     user_signed_in? && event.user == current_user
   end
-
 end
