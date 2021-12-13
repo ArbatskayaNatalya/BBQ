73c63d79d152cfae907441e0e3eedffbff356d68
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2021-12-13 15:33:37 +0300

    add comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,8 @@
 class Event < ApplicationRecord
-  belongs_to :user
   TITLE_MAX_LENGTH = 255
+
+  belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: TITLE_MAX_LENGTH }
   validates :address, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  resources :comments
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
     devise_for :users
     root "events#index"
-    resources :events
+
+    resources :events do
+      resources :comments, only: [:create, :destroy]
+    end
+
     resources :users, only: [:show, :edit, :update]
   end
 end
