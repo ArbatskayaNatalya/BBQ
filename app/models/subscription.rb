b97f348e477cbf5b709a7a970b4f0128019d8d6e
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2021-12-17 11:00:47 +0300

    refactor

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,8 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
+  before_validation :email_downcase
+
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }, unless: -> { user.present? }
 
@@ -9,12 +11,6 @@ class Subscription < ApplicationRecord
   validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
 
   validate :email_registered, unless: -> { user.present? }
-
-  def email_registered
-    if User.where(email: user_email).present?
-      errors.add(:user_email, :already_exists)
-    end
-  end
 
   def user_name
     if user.present?
@@ -29,6 +25,18 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def email_downcase
+    user_email&.downcase!
+  end
+
+  def email_registered
+    if User.where(email: user_email).present?
+      errors.add(:user_email, :already_exists)
     end
   end
 end
