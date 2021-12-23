84556568dec74602332311b94caad7bcc9ad09ba
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2021-12-23 15:28:53 +0300

    mailjet

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,5 @@ module Bbq
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.action_mailer.delivery_method = :mailjet
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'friendsbbq.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV["MAILJET_FROM"] }
 
   # Ошибки рассылки юзеру не показываем
   config.action_mailer.raise_delivery_errors = false
@@ -125,16 +125,6 @@ Rails.application.configure do
   # Делать рассылку писем (при false приложение только имитирует отправку)
   config.action_mailer.perform_deliveries = true
 
-  # Устанавливаем протокол, по которому отправлять (SMTP)
-  config.action_mailer.delivery_method = :smtp
-
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: 'plain',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
-  }
+  # Устанавливаем протокол, по которому отправлять
+  config.action_mailer.delivery_method = :mailjet
 end
