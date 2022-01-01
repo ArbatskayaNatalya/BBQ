8deff4bee0bef6a6c34d35652c5a425dfcfd59e0
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2022-01-01 11:51:41 +0300

    letter about comment

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -5,7 +5,7 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "t('text.new_subscription') #{event.title}"
+    mail to: event.user.email, subject: "#{t('text.new_subscription')} #{event.title}"
   end
 
 
@@ -13,13 +13,13 @@ class EventMailer < ApplicationMailer
     @comment = comment
     @event = event
 
-    mail to: email, subject: "t('text.new_comment') #{event.title}"
+    mail to: email, subject: "#{t('text.new_comment')} #{event.title}"
   end
 
   def photo(event, photo, email)
     @event = event
     @photo = photo
 
-    mail to: email, subject: "t('text.new_photo') #{event.title}"
+    mail to: email, subject: "#{t('text.new_photo')} #{event.title}"
   end
 end
