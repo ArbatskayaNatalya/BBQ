5e94dfa0feec08034ff30880980dec7b166e6601
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2022-01-04 20:50:37 +0300

    add yandex map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ import "channels"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 import 'stylesheets/application'
+import 'packs/scripts'
 
 Rails.start()
 ActiveStorage.start()
