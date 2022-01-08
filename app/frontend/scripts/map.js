53be25841954f954ba9f52db04d7206ed0bd3a08
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2022-01-08 13:05:33 +0300

    delete assets

--- app/frontend/scripts/map.js
+++ app/frontend/scripts/map.js
@@ -2,34 +2,34 @@ ymaps.ready(init);
 var myMap;
 
 function init(){
-  if (document.getElementById('map') !== null) {
-    address = document.getElementById('map').getAttribute('data-address');
+  if (document.getElementById('map') === null) return
 
-    myMap = new ymaps.Map("map", {
-      center: [55.76, 37.64],
-      zoom: 10
-    });
+  const address = document.getElementById('map').getAttribute('data-address');
 
-    myGeocoder = ymaps.geocode(address);
+  const myMap = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 10
+  });
 
-    myGeocoder.then(
-      function (res) {
-        coordinates = res.geoObjects.get(0).geometry.getCoordinates();
+  const myGeocoder = ymaps.geocode(address);
 
-        myMap.geoObjects.add(
-          new ymaps.Placemark(
-            coordinates,
-            {iconContent: address},
-            {preset: 'islands#blueStretchyIcon'}
-          )
-        );
+  myGeocoder.then(
+    function (res) {
+      coordinates = res.geoObjects.get(0).geometry.getCoordinates();
 
-        myMap.setCenter(coordinates);
-        myMap.setZoom(15);
-      }, function (err) {
-        alert('Ошибка при определении местоположения');
-      }
-    );
-  }
+      myMap.geoObjects.add(
+        new ymaps.Placemark(
+          coordinates,
+          {iconContent: address},
+          {preset: 'islands#blueStretchyIcon'}
+        )
+      );
+
+      myMap.setCenter(coordinates);
+      myMap.setZoom(15);
+    }, function (err) {
+      alert('Ошибка при определении местоположения');
+    }
+  );
 }
 
