1d823023c02a733b6fb5de66c052ec412499ef12
ArbatskayaNatalya <89268625+ArbatskayaNatalya@users.noreply.github.com>
2022-01-10 21:45:41 +0300

    add slider

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -9,10 +9,39 @@ import "channels"
 
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
+
 import '../stylesheets/application.scss'
 const images = require.context('../images', true)
+
 import '../scripts/map'
 import '../scripts/lightbox'
 
+import Glide from '@glidejs/glide'
+document.addEventListener('DOMContentLoaded', () => {
+  const glide = new Glide('.glide', {
+    type: 'carousel',
+    focustAt: 'center',
+    breakpoints: {
+        1840: {
+          perView: 8
+        },
+        1460: {
+          perView: 6
+        },
+        1180: {
+          perView: 5
+        },
+        700: {
+          perView: 4
+        },
+        360: {
+          perView: 3
+        }
+      }
+  })
+  glide.mount()
+})
+
 Rails.start()
 ActiveStorage.start()
+
